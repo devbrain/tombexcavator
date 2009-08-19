989e695b1c0e2fb9c688be63cb3ead000ac7ac48
igor.gutnik <igor.gutnik@00100026-7ed4-11de-b420-6b84dc2c4c10>
2009-08-19 13:37:37 +0000

    dl implemented

---
+++ trunk/src/abl/dl/dl.c
@@ -0,0 +1,80 @@
+#include <errno.h>
+#include <stdlib.h>
+
+#include "abl/dl/dl.h"
+
+#include "thirdparty/predef/predef.h"
+
+#if defined(PREDEF_PLATFORM_WIN32)
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+#if defined(PREDEF_PLATFORM_WIN32)
+typedef HMODULE handle_t;
+#else
+typedef void*  handle_t;
+#endif
+
+/* --------------------------------------------------------- */
+
+struct _shared_object_s
+{
+  handle_t handle;
+};
+
+/* --------------------------------------------------------- */
+
+shared_object_s* shared_object_open (const char* path)
+{
+  shared_object_s* result = 0;
+  handle_t         h;
+  if (!path)
+    {
+      return 0;
+    }
+  
+#if defined(PREDEF_PLATFORM_WIN32)
+  h = LoadLibrary (path);
+  if (!h)
+    {
+      return 0;
+    }
+#else
+  h = dlopen (path, RTLD_LAZY);
+  if (!h)
+    {
+      return 0;
+    }
+#endif
+  result = (shared_object_s*)malloc (sizeof (shared_object_s));
+  result->handle = h;
+  return result;
+}
+/* --------------------------------------------------------- */
+void* shared_object_get_symbol (shared_object_s* so, const char* name)
+{
+  if (!so || !so->handle)
+    {
+      return 0;
+    }
+#if defined(PREDEF_PLATFORM_WIN32)
+  return GetProcAddress (so->handle, name);
+#else
+  return dlsym (so->handle, name);
+#endif
+}
+/* --------------------------------------------------------- */
+void shared_object_close (shared_object_s* so)
+{
+  if (!so || !so->handle)
+    {
+      return;
+    }
+#if defined(PREDEF_PLATFORM_WIN32)
+  FreeLibrary (so->handle);
+#else
+  dlclose (so->handle);
+#endif
+}
