5518a58cfa8c84c1b4c7174ccf00e309215d7ace
igor.gutnik <igor.gutnik@00100026-7ed4-11de-b420-6b84dc2c4c10>
2010-01-04 14:36:30 +0000

    dynamic loader implemented

--- trunk/src/abl/dynamic_loader.cpp
+++ trunk/src/abl/dynamic_loader.cpp
@@ -1,13 +1,51 @@
+#include "abl/dynamic_loader.hpp"
+#include "abl/dynamic_library.hpp"
+
+#include <map>
 
 namespace abl
 {
-  dynamic_library_c ();
+  typedef std::pair <std::string, std::string> so_path_t;
+  typedef std::map <so_path_t, dynamic_library_c*> so_map_t;
 
-    ~dynamic_library_c ();
-
-    bool load (const path_c& path, const std::string& name);
-    std::string name () const;
-    std::string qualified_name () const;
-    std::string path () const;
-    void* load_symbol (const std::string& symbol);
+  struct dynamic_loader_impl_s
+  {
+    std::string m_root;
+    so_map_t    m_map;
+  };
+  // ----------------------------------------------------------------
+  dynamic_loader_c::dynamic_loader_c (const std::string& so_dir)
+  {
+    m_pimpl = new dynamic_loader_impl_s;
+    m_pimpl->m_root = so_dir;
+  }
+  // ----------------------------------------------------------------
+  dynamic_loader_c::~dynamic_loader_c ()
+  {
+    for (so_map_t::iterator i = m_pimpl->m_map.begin (); i != m_pimpl->m_map.end (); i++)
+      {
+	delete i->second;
+      }
+    delete m_pimpl;
+  }
+  // ----------------------------------------------------------------
+  dynamic_library_c* dynamic_loader_c::load (const std::string& name)
+  {
+    return this->load (m_pimpl->m_root, name);
+  }
+  // ----------------------------------------------------------------
+  dynamic_library_c* dynamic_loader_c::load (const std::string& so_dir, const std::string& name)
+  {
+    so_path_t id (so_dir, name);
+    so_map_t::iterator i = m_pimpl->m_map.find (id);
+    if (i != m_pimpl->m_map.end ())
+      {
+	dynamic_library_c* res = i->second;
+	res->_duplicate ();
+	return res;
+      }
+    dynamic_library_c* res = new dynamic_library_c (so_dir, name);
+    m_pimpl->m_map.insert (so_map_t::value_type (id, res));
+    return res;
+  }
 }
--- trunk/src/abl/dynamic_loader.hpp
+++ trunk/src/abl/dynamic_loader.hpp
@@ -6,59 +6,21 @@
 
 namespace abl
 {
-  class path_c;
+  class dynamic_library_c;
+  struct dynamic_loader_impl_s;
 
-  class dynamic_loader_c;
-  
-  struct dynamic_library_impl_s;
-  /**
-   * This class represents a dynamic library
-   */
-  class ABL_API dynamic_library_c
+  class ABL_API dynamic_loader_c
   {
-    friend class dynamic_loader_c;
   public:
-    dynamic_library_c ();
-
-    ~dynamic_library_c ();
-
-    bool load (const path_c& path, const std::string& name);
-    /** 
-     * returns the name of the library. For example, for /a/libX.so it returns X
-     * 
-     * 
-     * @return base name of the shared object
-     */
-    std::string name () const;
-    /** 
-     * returns the full name of the library. For example, for /a/libX.so it returns libX.so
-     * 
-     * 
-     * @return system dependent name of the library
-     */
-    std::string qualified_name () const;
-    /** 
-     * returns path to the library. For example, for /a/libX.so it returns /a
-     * 
-     * 
-     * @return path where the library resides
-     */
-    std::string path () const;
-
-    /** 
-     * loads the symbol from the library
-     * 
-     * @param symbol - name of the symbol to load
-     * 
-     * @return pointer to the loaded symbol or NULL in case of failure
-     */
-    void* load_symbol (const std::string& symbol);
+    dynamic_loader_c (const std::string& so_dir);
+    ~dynamic_loader_c ();
+    
+    dynamic_library_c* load (const std::string& name);
+    dynamic_library_c* load (const std::string& so_dir, const std::string& name);
   private:
-    dynamic_library_c ();
-    ~dynamic_library_c ();
-  private:
-    dynamic_library_impl_s* m_pimpl;
+    dynamic_loader_impl_s* m_pimpl;
   };
+
 }
 
 #endif
