cc86223a1d58cb6fb3c303f7814bf6eeb1d79b1b
igor.gutnik <igor.gutnik@00100026-7ed4-11de-b420-6b84dc2c4c10>
2010-07-14 09:45:47 +0000

    rollback

--- trunk/abl/unicode/unicode_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ABL_UNICODE_UNICODE_TYPES_H__
-#define __ABL_UNICODE_UNICODE_TYPES_H__
-
-#include "abl/types/c_types.h"
-
-typedef te_uint16_t ucs16_t;
-typedef te_uint32_t ucs4_t;
-
-#endif
