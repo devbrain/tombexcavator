17818547cbbd685f375293b90fcc6d4b2700abc0
Igor <igor.gutnik@gmail.com>
2020-02-23 18:09:51 +0200

    shell started

---
+++ src/shell/main.cc
@@ -0,0 +1,43 @@
+#include <linenoise/linenoise.hpp>
+const auto path = "history.txt";
+
+
+int main(int argc, char* argv[])
+{
+    // Setup completion words every time when a user types
+    linenoise::SetCompletionCallback([](const char* editBuffer, std::vector<std::string>& completions) {
+        if (editBuffer[0] == 'h') {
+            completions.push_back("hello");
+            completions.push_back("hello there");
+        }
+    });
+
+    // Enable the multi-line mode
+    linenoise::SetMultiLine(true);
+
+// Set max length of the history
+    linenoise::SetHistoryMaxLen(4);
+
+// Load history
+    linenoise::LoadHistory(path);
+
+    while (true) {
+        // Read line
+        std::string line;
+        auto quit = linenoise::Readline("hello> ", line);
+
+        if (quit) {
+            break;
+        }
+
+        std::cout <<  "echo: '" << line << "'" << std::endl;
+
+        // Add text to history
+        linenoise::AddHistory(line.c_str());
+    }
+
+// Save history
+    linenoise::SaveHistory(path);
+
+    return 0;
+}
