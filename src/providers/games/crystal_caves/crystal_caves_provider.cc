dbc0f64969d40a6a8a9c422ae8ce3c96546e02da
Igor <igor.gutnik@gmail.com>
2021-03-11 11:35:58 +0200

    cosmetic changes

--- src/providers/games/crystal_caves/crystal_caves_provider.cc
+++ src/providers/games/crystal_caves/crystal_caves_provider.cc
@@ -8,29 +8,27 @@
 #include <tomb-excavator/games/common/loaders/picture_loader.hh>
 #include <tomb-excavator/games/apogee/prographx_sprite_loader.hh>
 
-class ccfs : public games::common::flat_provider
-{
-public:
-    ccfs();
-};
-
-// ----------------------------------------------------------------------------------------
 using namespace apogee;
 using namespace games::common;
 
-ccfs::ccfs()
-        : games::common::flat_provider("Crystal Caves",
-                                       {
-                                               new prographx_sprite_loader("sprites", "cc1.gfx", 0, 0),
-                                               new prographx_sprite_loader("menu-tiles", "cc1-f1.mni", 0, 0),
-                                               new picture_loader("apogee", "cc1.apg"),
-                                               new picture_loader("title", "cc1.ttl"),
-                                               new picture_loader("end", "cc1.end"),
-                                               new picture_loader("credits", "cc1.cdt")
-                                       }
-)
+class ccfs : public flat_provider
 {
+public:
+    ccfs()
+            : flat_provider("Crystal Caves",
+                            {
+                                    new prographx_sprite_loader("sprites", "cc1.gfx", 0, 0),
+                                    new prographx_sprite_loader("menu-tiles", "cc1-f1.mni", 0, 0),
+                                    new picture_loader("apogee", "cc1.apg"),
+                                    new picture_loader("title", "cc1.ttl"),
+                                    new picture_loader("end", "cc1.end"),
+                                    new picture_loader("credits", "cc1.cdt")
+                            }
+    )
+    {
 
-}
+    }
+};
+
 // ----------------------------------------------------------------------------------------
 REGISTER_PROVIDER_FS(ccfs)
